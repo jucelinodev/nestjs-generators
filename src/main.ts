184f5fb889ec045318e389395aa233db406139d0
Jucelino Barbosa <jucelino.barbosa.t@uds.com.br>
2024-02-24 15:23:58 -0300

    add mongo db conection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import * as compression from 'compression';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,6 +24,15 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   const configService = app.get(ConfigService);
+
+  const mongoUri = configService.get<string>('mongoUri');
+
+  const logger = new Logger('Database');
+  mongoose
+    .connect(mongoUri)
+    .then(() => logger.log('Mongo conectado com sucesso'))
+    .catch((e) => logger.error(e));
+
   const port = configService.get<number>('port');
   await app.listen(port);
 }
--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,4 @@
 export const configuration = () => ({
   port: parseInt(process.env.PORT, 10) || 3333,
+  mongoUri: process.env.MONGO_URI,
 });
