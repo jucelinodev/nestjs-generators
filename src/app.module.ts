7b48c0a567cd8fd2593f86829ca65359b8bd3b6d
Jucelino Barbosa <jucelino.barbosa.t@uds.com.br>
2024-02-24 16:56:00 -0300

    remove books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
-import { BookModule } from './books/book.module';
 import { HealthModule } from './shared/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './shared/config';
 
 @Module({
   imports: [
+    HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
     }),
-    HealthModule,
-    BookModule,
   ],
   controllers: [],
   providers: [],
